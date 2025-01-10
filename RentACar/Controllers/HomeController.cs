a266a62a3d9e0ca2c2445f085489db6b36a299ae
rachev3 <ddrachev123@gmail.com>
2025-01-10 14:56:34 +0200

    Register and Login working

--- RentACar/Controllers/HomeController.cs
+++ RentACar/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using RentACar.Models;
+using RentACar.ViewModels.User;
 
 namespace RentACar.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _logger = logger;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -18,6 +24,57 @@ namespace RentACar.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            User newUser = new()
+            {
+                UserName = model.UserName,
+                FirstName = model.FirstName,
+                MiddleName = model.MiddleName,
+                LastName = model.LastName,
+                Email = model.Email
+            };
+            var user = await userManager.FindByEmailAsync(model.Email) ?? await userManager.FindByNameAsync(model.UserName);
+
+            if (user == null)
+            {
+                var createUser = await userManager.CreateAsync(newUser, model.Password);
+                if (createUser.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newUser, "Client");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.UserName, model.Password, false, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Невалиден опит за влизане.");
+            }
+
+            return View(model);
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ RentACar/Seeders/RolesAndAdminSeeder.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RentACar.Models;
+
+namespace RentACar.Seeders
+{
+    public class RolesAndAdminSeeder
+    {
+        public static async Task Initialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string[] roleNames = { "Admin", "Client" };   // списък с роли
+
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)      // добавя ролите ако не съществуват
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            var adminUser = new User
+            {
+                UserName = "Administrator",
+                FirstName = "Admin",
+                MiddleName = "Adminov",
+                LastName = "Adminov",
+                Email = "admin@example.com",
+            };
+         
+            string adminPassword = "Admin@123";
+           
+            var admin = await userManager.FindByEmailAsync("admin@example.com");
+
+            //проверява дали съшествуват такъв потребител и го създава ако не съществува
+
+            if (admin == null)
+            {
+                var createPowerUser = await userManager.CreateAsync(adminUser, adminPassword);
+                if (createPowerUser.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+          
+        }
+    }
+}
