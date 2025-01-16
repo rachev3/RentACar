e231fa10d4ca87f93116aab79dc565b7e9dbbcc8
rachev3 <ddrachev123@gmail.com>
2025-01-16 23:44:00 +0200

    cars show in user intefrace

---
+++ RentACar/Controllers/CarController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACar.Services.Interfaces;
+using RentACar.ViewModels.Car;
+
+namespace RentACar.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = await _carService.GetAll();
+            return View(viewModel);
+        }
+    }
+}
